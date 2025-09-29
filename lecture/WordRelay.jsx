396c7403bc014f6545d37cc218ad7f0b5719f557
유재건 <yjk9805@naver.com>
2025-09-29 19:35:44 +0900

    feat: wordRelay Class 컴포넌트 구현

--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -3,11 +3,50 @@ const { Component } = React;
 
 class WordRelay extends React.Component {
   state = {
-    text: "Hello, World!",
+    word: "유재건",
+    value: "",
+    result: "",
   };
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        result: "딩동댕",
+        word: this.state.value,
+        value: "",
+      });
+      this.input.focus();
+    } else {
+      this.setState({
+        result: "땡",
+        value: "",
+      });
+    }
+  };
+
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+  onRefInput = (c) => {
+    this.input = c;
+  };
   render() {
-    return <div>{this.state.text}</div>;
+    return (
+      <div>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            ref={this.onRefInput}
+            type="text"
+            value={this.state.value}
+            onChange={this.onChangeInput}
+          />
+          <button>입력!</button>
+        </form>
+        <div>{this.state.result}</div>
+      </div>
+    );
   }
 }
 
